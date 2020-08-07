0f667be149299a247be0ce83013c6708f00d507b
innocentdavid <paulinnocent04@gmail.com>
2020-08-07 23:30:19 +0100

    ud

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Staff)
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(History)
+admin.site.register(Notification)
--- bank/models.py
+++ bank/models.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class Staff(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    role = models.CharField(max_length=200, default="manager")
+    totalCustomer = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name.username
+        
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    fname = models.CharField(max_length=200, null=True)
-    lname = models.CharField(max_length=200, null=True)
+    manager = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
     email = models.CharField(max_length=200, null=True)
     address = models.CharField(max_length=200, null=True)
     dob = models.CharField(max_length=200, null=True)
@@ -12,16 +21,7 @@ class Customer(models.Model):
     def __str__(self):
         return self.fname
 
-class Staff(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    role = models.CharField(max_length=200, default="manager")
-    totalCustomer = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
 class Account(models.Model):
-    manager = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
     customer = models.ManyToManyField("Customer", related_name="emails_received")
     accountNum = models.IntegerField(null=True, blank=True)
     accountType = models.CharField(default="savings", max_length=200)
@@ -30,7 +30,7 @@ class Account(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.accountNum
+        return f"{self.accountNum}"
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -39,13 +39,23 @@ class Category(models.Model):
         return self.name
 
 class History(models.Model):
+    INCOME = 'income'
+    EXPENDITURE = 'expenditure'
+    
+    TRANSC_TYPE =(
+      (INCOME,'INCOME'),
+      (EXPENDITURE,'EXPENDITURE')
+      )
     account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     # type = income (True) or expenditure (False)
-    transcType = models.BooleanField(default=False)
+    transcType = models.CharField(max_length=20, choices=TRANSC_TYPE)
     amount = models.FloatField(default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True)
     seen = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.transcType}"
 
 class Notification(models.Model):
-    
+    pass
