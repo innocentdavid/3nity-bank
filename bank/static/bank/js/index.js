0851e140bfc31c3ac560b6150b06381b7826d5a6
innocentdavid <paulinnocent04@gmail.com>
2020-08-07 19:22:41 +0100

    up

--- bank/static/bank/js/index.js
+++ bank/static/bank/js/index.js
@@ -1,12 +1,25 @@
 function closePopModal() {
-    document.querySelector('.pop-model').style.display='none';
+  document.querySelector('.pop-model').style.display = 'none';
 }
 
 function rTransc(id) {
-    document.querySelector('.pop-model').style.display='block';
-    document.querySelector('.modal-body').innerHTML=id;
-
+  document.querySelector('.pop-model').style.display = 'block';
+  document.querySelector('.modal-body').innerHTML = id;
 }
-window.addEventListener('DOMContentLoaded', function () {
-    
-})
+
+function pageToggle(page) {
+
+  fetch('/page/'+page, {
+    method: 'POST',
+    body: JSON.stringify({
+      file_name: 'page'
+    })
+  })
+  .then(response => response.json())
+  .then(response => {
+    // console.log(response);
+    document.querySelector('#main-body').innerHTML = response.page;
+  });
+}
+
+window.addEventListener('DOMContentLoaded', function () {})
--- bank/views.py
+++ bank/views.py
@@ -1,4 +1,6 @@
 import json
+from django.core.files.storage import default_storage
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
@@ -12,4 +14,22 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'bank/index.html')
+  pdToggle = 'profile'
+  pdT='Profile'
+  
+  context={'pdToggle':pdToggle, 'pdT':pdT}
+  return render(request, 'bank/index.html', context)
+  
+def profile(request):
+  pdToggle = ''
+  pdT='Dashboard'
+  
+  context={'pdToggle':pdToggle, 'pdT':pdT}
+  return render(request, 'bank/profile.html', context)
+
+@csrf_exempt
+def page(request, file_name):
+  f = default_storage.open(f"bank/templates/bank/{file_name}.html")
+  page = f.read().decode("utf-8")
+  
+  return JsonResponse({"page":page}, status=200)
