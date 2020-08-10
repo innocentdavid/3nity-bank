201547f192c6b56bc95a95950e8e532d9a93d760
innocent <paulinnocent04@gmail.com>
2020-08-10 11:15:20 +0100

    df

--- bank/static/bank/js/layout.js
+++ bank/static/bank/js/layout.js
@@ -24,8 +24,9 @@ function getPage(page) {
 
       let style = `<link rel="stylesheet" href="././static/bank/css/${page}.css">`;
       $('#customCss').html(style);
-      let js = `<script src="././static/bank/js/${page}.js">`;
-      $('#customJs').html(js);
+      // js not working
+      // let js = `<script src="././static/bank/js/${page}.js">`;
+      // $('body').append(js);
 
       let pdToggle = '<a href="/" style="color:white; padding: 0 .3rem; border:1px solid; font-size: 1rem;">';
       pdToggle += '<i class="fa fa-arrow-left"></i><span> Dashboard </span></a>';
@@ -47,3 +48,88 @@ window.addEventListener('DOMContentLoaded', function () {
 window.onpopstate = function(event) {
   getPage(event.state.page);
 }
+
+
+// profile js start
+
+function addSrc() {
+  $('.add-exp').css('background', 'rgb(17, 184, 128)');
+  $('.add-src').css('background', 'black');
+  $('.add-exp-box').css('display', 'none');
+  $('.toggle-add-box').css('display', 'block');
+  $('.add-src-box').css('display', 'block');
+}
+function addExp() {
+  $('.add-src').css('background', 'rgb(17, 184, 128)');
+  $('.add-exp').css('background', 'black');
+  $('.add-src-box').css('display', 'none');
+  $('.toggle-add-box').css('display', 'block');
+  $('.add-exp-box').css('display', 'block');
+}
+function toggleAddBox() {
+  $('.add-exp').css('background', 'rgb(17, 184, 128)');
+  $('.add-src').css('background', 'rgb(17, 184, 128)');
+  $('.add-exp-box').css('display', 'none');
+  $('.add-src-box').css('display', 'none');
+  $('.toggle-add-box').css('display', 'none');
+}
+
+function expandCatg(catgId) {
+  document.getElementsByClassName('cr'+catgId)[0].style.display = 'none';
+  document.getElementsByClassName('cd'+catgId)[0].style.display = 'block';
+  document.getElementsByClassName('catgSumBox'+catgId)[0].style.display = 'block';
+}
+function compressCatg(catgId) {
+  document.getElementsByClassName('cd'+catgId)[0].style.display = 'none';
+  document.getElementsByClassName('cr'+catgId)[0].style.display = 'block';
+  document.getElementsByClassName('catgSumBox'+catgId)[0].style.display = 'none';
+}
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'total expenditure'],
+    ['Work', 40],
+    ['Nap', 10],
+    ['Eat', 11],
+    ['Eat', 11],
+    ['Watch TV', 14],
+    ['Watch TV', 14],
+    ['Sleep', 15]
+  ]);
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, {
+    title: 'My Weekly Expenditures',
+    is3D: true,
+  });
+}
+
+google.charts.load('current', {
+  'packages': ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+$(document).ready(function () {
+})
+
+// profile js end
+
+// transfer js start
+
+// let accNum = document.querySelector('#accountNum');
+// alert(accNum);
+// if (accNum != Null){
+//   accNum.addEventListener('keyup',function () {
+//     alert('keyup');
+//   })
+// }
+
+function checkAccNum() {
+  let accNum = document.querySelector('#accountNum').value;
+  if (accNum.length >= 10){
+    // send a fetch requsts
+  }
+}
+
+// transfer js end
